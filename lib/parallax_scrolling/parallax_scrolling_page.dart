eefecedea520f763dc998e764e4b73ec852683e6
Vitor Lucas Pires Cordovil <lukassfifa@gmail.com>
2021-06-13 13:23:49 -0300

    Adds the parallax animation to the images list

--- lib/parallax_scrolling/parallax_scrolling_page.dart
+++ lib/parallax_scrolling/parallax_scrolling_page.dart
@@ -13,9 +13,15 @@ class ParallaxScrollingPage extends StatelessWidget {
             _imgsList.length,
             (index) {
               return Center(
-                child: AspectRatio(
-                  aspectRatio: 16 / 9,
-                  child: ImageTile(_imgsList[index]),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 8.0,
+                    vertical: 16,
+                  ),
+                  child: AspectRatio(
+                    aspectRatio: 16 / 9,
+                    child: ImageTile(_imgsList[index]),
+                  ),
                 ),
               );
             },
@@ -40,71 +46,212 @@ class _ImageTileState extends State<ImageTile> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
+    return LayoutBuilder(
+      builder: (context, constraints) => Center(
         child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(16),
-              child: Image.network(
-                image.url,
-                fit: BoxFit.cover,
-                frameBuilder: (context, child, frame, wasSynchronouslyLoaded) {
-                  return Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      child,
-                      Positioned(
-                        left: 15,
-                        bottom: 30,
-                        child: Column(
-                          children: [
-                            Text(
-                              image.title,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline6!
-                                  .copyWith(
-                                    color: Colors.white,
-                                  ),
-                            ),
-                            Text(
-                              image.subTitle,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline6!
-                                  .copyWith(
-                                    color: Colors.white,
-                                    fontSize: 12,
-                                  ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  );
-                },
-                loadingBuilder: (BuildContext context, Widget child,
-                    ImageChunkEvent? loadingProgress) {
-                  if (loadingProgress == null) {
-                    return child;
-                  }
-                  return Container(
-                    color: Colors.blueGrey.withAlpha(10),
-                    child: Center(
-                      child: CircularProgressIndicator(
-                        value: loadingProgress.expectedTotalBytes != null
-                            ? loadingProgress.cumulativeBytesLoaded /
-                                loadingProgress.expectedTotalBytes!
-                            : null,
-                      ),
-                    ),
-                  );
-                },
+          width: double.infinity,
+          height: double.infinity,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                BackgroundImage(image),
+                MyGradient(),
+                Positioned(
+                  left: constraints.maxWidth * 0.05,
+                  bottom: constraints.maxHeight * 0.02,
+                  width: constraints.maxWidth * 0.35,
+                  height: constraints.maxHeight * 0.2,
+                  child: TitleWithSubTitle(image: image),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TitleWithSubTitle extends StatelessWidget {
+  TitleWithSubTitle({
+    required this.image,
+  }) : super(key: ValueKey(image.url));
+
+  final ImageData image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 4,
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: Text(
+              image.title,
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: FittedBox(
+            fit: BoxFit.contain,
+            child: Text(
+              image.subTitle,
+              style: Theme.of(context).textTheme.headline6!.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class BackgroundImage extends StatelessWidget {
+  BackgroundImage(this.image) : super(key: UniqueKey());
+  final GlobalKey _backgroundImageKey = GlobalKey();
+  final ImageData image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flow(
+      delegate: ParallaxFlowDelegate(
+        backgroundImageKey: _backgroundImageKey,
+        listItemContext: context,
+        scrollable: Scrollable.of(context)!,
+      ),
+      children: [
+        Image.network(
+          image.url,
+          key: _backgroundImageKey,
+          fit: BoxFit.cover,
+          loadingBuilder: (
+            BuildContext context,
+            Widget child,
+            ImageChunkEvent? loadingProgress,
+          ) {
+            if (loadingProgress == null) {
+              return child;
+            }
+            return Center(
+              child: CircularProgressIndicator(
+                value: loadingProgress.expectedTotalBytes != null
+                    ? loadingProgress.cumulativeBytesLoaded /
+                        loadingProgress.expectedTotalBytes!
+                    : null,
               ),
-            )),
+            );
+          },
+          frameBuilder: (
+            BuildContext context,
+            Widget child,
+            int? frame,
+            bool wasSynchronouslyLoaded,
+          ) {
+            if (wasSynchronouslyLoaded) {
+              return child;
+            }
+            return AnimatedOpacity(
+              child: child,
+              opacity: frame == null ? 0 : 1,
+              duration: const Duration(seconds: 1),
+              curve: Curves.easeOut,
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class ParallaxFlowDelegate extends FlowDelegate {
+  ParallaxFlowDelegate({
+    required this.scrollable,
+    required this.listItemContext,
+    required this.backgroundImageKey,
+  }) : super(repaint: scrollable.position);
+
+  final ScrollableState scrollable;
+  final BuildContext listItemContext;
+  final GlobalKey backgroundImageKey;
+
+  @override
+  BoxConstraints getConstraintsForChild(int i, BoxConstraints constraints) {
+    return BoxConstraints.tightFor(
+      width: constraints.maxWidth,
+    );
+  }
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    // Calculates the position of this list item within the viewport.
+    final scrollableBox = scrollable.context.findRenderObject() as RenderBox;
+    final listItemBox = listItemContext.findRenderObject() as RenderBox;
+    final listItemOffset = listItemBox.localToGlobal(
+      listItemBox.size.centerLeft(Offset.zero),
+      ancestor: scrollableBox,
+    );
+
+    // Determines the percent position of this list item within the
+    // scrollable area.
+    final viewportDimension = scrollable.position.viewportDimension;
+    final scrollFraction =
+        (listItemOffset.dy / viewportDimension).clamp(0.0, 1.0);
+
+    // Calculates the vertical alignment of the background
+    // based on the scroll percentage.
+    final verticalAlignment = Alignment(0.0, scrollFraction * 2 - 1);
+
+    // Convert the background alignment into a pixel offset for
+    // painting purposes.
+    final backgroundSize =
+        (backgroundImageKey.currentContext!.findRenderObject() as RenderBox)
+            .size;
+    final listItemSize = context.size;
+    final childRect =
+        verticalAlignment.inscribe(backgroundSize, Offset.zero & listItemSize);
+
+    // Paint the background.
+    context.paintChild(
+      0,
+      transform: Transform.translate(
+        offset: Offset(0.0, childRect.top),
+      ).transform,
+    );
+  }
+
+  @override
+  bool shouldRepaint(ParallaxFlowDelegate oldDelegate) {
+    return scrollable != oldDelegate.scrollable ||
+        listItemContext != oldDelegate.listItemContext ||
+        backgroundImageKey != oldDelegate.backgroundImageKey;
+  }
+}
+
+class MyGradient extends StatelessWidget {
+  const MyGradient({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [
+              Colors.transparent,
+              Colors.black.withOpacity(0.7),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            stops: [0.6, 0.95]),
       ),
     );
   }
@@ -119,14 +266,14 @@ class ImageData {
 
 final _imgsList = const <ImageData>[
   ImageData(
-    'https://hdwallsource.com/img/2014/8/wonderful-41060-42031-hd-wallpapers.jpg',
-    'Bridge',
-    'Switzerland',
-  ),
-  ImageData(
     'https://www.extremetech.com/wp-content/uploads/2016/12/ngc6357.jpg',
     'X II X!',
     'Space',
+  ),
+  ImageData(
+    'https://user-images.githubusercontent.com/9553743/121814852-91756180-cc49-11eb-9619-d824811b52a6.jpg',
+    'The Void Walker',
+    'Kassadin',
   ),
   ImageData(
     'https://www.extremetech.com/wp-content/uploads/2016/12/heic1007a.jpg',
@@ -154,9 +301,9 @@ final _imgsList = const <ImageData>[
     'Dragon\'s Cave',
   ),
   ImageData(
-    'https://i.imgur.com/gMWCNBr.jpg',
-    'Black Hole',
-    'Interstellar 2014',
+    'https://user-images.githubusercontent.com/9553743/121814750-1449ec80-cc49-11eb-9ece-62e2eeb79ef7.jpg',
+    'Les châteaux de votres rêves',
+    'French',
   ),
   ImageData(
     'https://i.redd.it/0ac253vmug471.png',
