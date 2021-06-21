6ac4a016a5c879cda4af96e55d45ae4feb34a239
Vitor Lucas Pires Cordovil <lukassfifa@gmail.com>
2021-06-21 00:02:54 -0300

    plans the week

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:june_design_challenges/derolez_portifolio/derolez_page.dart';
+import 'package:june_design_challenges/light_ray/light_ray_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const DerolezPage(),
+      home: const LightRayPage(),
     );
   }
 }
