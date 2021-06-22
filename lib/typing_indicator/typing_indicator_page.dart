513a1c2712893884e9868937cb88692dedb35752
Vitor Lucas Pires Cordovil <lukassfifa@gmail.com>
2021-06-22 20:42:43 -0300

    Adds the initial code for the implementation of the typing indicator.
    Obs.: I was stucked when there was needed to animate the Scroll accordingly to the state of the SwitchButton value.

---
+++ lib/typing_indicator/typing_indicator_page.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+class TypingIndicatorPage extends StatefulWidget {
+  const TypingIndicatorPage({Key? key}) : super(key: key);
+
+  @override
+  State<TypingIndicatorPage> createState() => _TypingIndicatorPageState();
+}
+
+class _TypingIndicatorPageState extends State<TypingIndicatorPage> {
+  bool showIndicator = false;
+  final _controller = ScrollController();
+
+  void onChanged(bool val) {
+    setState(() {
+      showIndicator = val;
+    });
+
+    if (val)
+      WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+        _controller.animateTo(
+          _controller.position.maxScrollExtent,
+          duration: Duration(milliseconds: 300),
+          curve: Curves.linear,
+        );
+      });
+  }
+
+  List<Widget> listWidgets() {
+    return [
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: 150,
+          height: 100,
+          color: Colors.green.shade400,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: 150,
+          height: 30,
+          color: Colors.grey.shade400,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: 150,
+          height: 100,
+          color: Colors.red.shade400,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: 150,
+          height: 30,
+          color: Colors.grey.shade400,
+        ),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: 150,
+          height: 100,
+          color: Colors.grey.shade400,
+        ),
+      ),
+    ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Typing Indicator'),
+        centerTitle: true,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                color: Colors.black54,
+                child: ScrollConfiguration(
+                  behavior: ScrollConfiguration.of(context).copyWith(
+                    dragDevices: {
+                      PointerDeviceKind.mouse,
+                      PointerDeviceKind.touch,
+                    },
+                  ),
+                  child: SingleChildScrollView(
+                    controller: _controller,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 16.0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          ...listWidgets(),
+                          if (showIndicator) TypingIndicator(),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 50,
+              child: Center(
+                child: Switch(value: showIndicator, onChanged: onChanged),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TypingIndicator extends StatefulWidget {
+  const TypingIndicator({Key? key}) : super(key: key);
+
+  @override
+  State<TypingIndicator> createState() => _TypingIndicatorState();
+}
+
+class _TypingIndicatorState extends State<TypingIndicator>
+    with SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 60,
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            width: 100,
+            color: Colors.grey.shade700,
+          ),
+        ),
+      ),
+    );
+  }
+}
