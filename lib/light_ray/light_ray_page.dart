4e5d006f24878e77d0294cf443ee351aa4d636cc
Vitor Lucas Pires Cordovil <lukassfifa@gmail.com>
2021-06-21 23:08:57 -0300

    Animates the ray lights

--- lib/light_ray/light_ray_page.dart
+++ lib/light_ray/light_ray_page.dart
@@ -16,12 +16,9 @@ class _LightRayPageState extends State<LightRayPage>
   @override
   void initState() {
     super.initState();
-    controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 1),
-    );
-    // controller.repeat();
-    controller.forward();
+    controller = AnimationController(vsync: this);
+    controller.repeat();
+    // controller.forward();
   }
 
   @override
@@ -62,16 +59,14 @@ class _LightRayPageState extends State<LightRayPage>
 }
 
 class LightRayPainter extends CustomPainter {
-  LightRayPainter() {
-    print('Constructor called');
-  }
+  static List<LightRay> lightRayList = [];
+
   @override
   void paint(Canvas canvas, Size size) {
     final fullRect = Rect.fromLTWH(0, 0, size.width, size.height);
     canvas.drawRect(
       fullRect,
       Paint()..color = const Color(0xFFe4e027),
-      // Paint()..color = const Color(0xFF000000),
     );
 
     canvas.saveLayer(
@@ -89,74 +84,11 @@ class LightRayPainter extends CustomPainter {
   //Aumentando e diminuindo o raio de luz com base no valor do controller.
   //III.
   void drawRays(Canvas canvas, Size size) {
-    final center = Offset(size.width / 2, size.height / 2);
-
-    final lightRayList = <LightRay>[];
-    double innerRaySize = 1;
-    final rand = Random();
-
-    double numberOfLightRays = 6;
-    //In percentage of a full rotation (2*pi)
-    double initialAngle = 0;
-
-    for (int i = 0; i < numberOfLightRays; i++) {
-      final radiusRandomness = rand.nextDouble();
-      final randomRadius = (size.shortestSide * .25) +
-          size.shortestSide * 0.3 * radiusRandomness;
-
-      double angle =
-          initialAngle + 100 / numberOfLightRays * (i) - innerRaySize;
-      lightRayList.add(
-        LightRay(
-          center: center,
-          startAngle: angle,
-          endAngle: angle + innerRaySize * 2,
-          radius: randomRadius,
-          innerRaySize: innerRaySize,
-        ),
-      );
-    }
-    numberOfLightRays = 9;
-    initialAngle = 3;
-
-    for (int i = 0; i < numberOfLightRays; i++) {
-      final radiusRandomness = rand.nextDouble();
-      final randomRadius = (size.shortestSide * .25) +
-          size.shortestSide * 0.3 * radiusRandomness;
-
-      double angle =
-          initialAngle + 100 / numberOfLightRays * (i) - innerRaySize;
-      lightRayList.add(
-        LightRay(
-          center: center,
-          startAngle: angle,
-          endAngle: angle + innerRaySize * 2,
-          radius: randomRadius,
-          innerRaySize: innerRaySize,
-        ),
-      );
-    }
-
-    numberOfLightRays = 9;
-    initialAngle = 10;
-
-    for (int i = 0; i < numberOfLightRays; i++) {
-      final radiusRandomness = rand.nextDouble();
-      final randomRadius = (size.shortestSide * .25) +
-          size.shortestSide * 0.3 * radiusRandomness;
-
-      double angle =
-          initialAngle + 100 / numberOfLightRays * (i) - innerRaySize;
-      lightRayList.add(
-        LightRay(
-          center: center,
-          startAngle: angle,
-          endAngle: angle + innerRaySize * 2,
-          radius: randomRadius,
-          innerRaySize: innerRaySize,
-        ),
-      );
-    }
+    if (lightRayList.isEmpty) {
+      print('here');
+      loadRayPaths(size);
+    } else
+      updateRays(size);
 
     final paint = Paint()
       ..color = Color(0xFF070707)
@@ -183,6 +115,90 @@ class LightRayPainter extends CustomPainter {
           stops: [0.1, 0.9],
         ).createShader(fullRect),
     );
+  }
+
+  void updateRays(Size size) {
+    for (int i = 0; i < lightRayList.length; i++)
+      lightRayList[i].increaseRadius(
+        10,
+        size.shortestSide * .55,
+        size.shortestSide * .15,
+      );
+  }
+
+  void loadRayPaths(Size size) {
+    final center = Offset(size.width / 2, size.height / 2);
+
+    double innerRaySize = 1;
+    final rand = Random();
+
+    double numberOfLightRays = 6;
+    //In percentage of a full rotation (2*pi)
+    double initialAngle = 0;
+
+    for (int i = 0; i < numberOfLightRays; i++) {
+      final radiusRandomness = rand.nextDouble();
+      final randomRadius = (size.shortestSide * .15) +
+          size.shortestSide * 0.3 * radiusRandomness;
+
+      double angle =
+          initialAngle + 100 / numberOfLightRays * (i) - innerRaySize;
+      lightRayList.add(
+        LightRay(
+          center: center,
+          startAngle: angle,
+          endAngle: angle + innerRaySize * 2,
+          radius: randomRadius,
+          innerRaySize: innerRaySize,
+          isGrowing: rand.nextBool(),
+        ),
+      );
+    }
+    numberOfLightRays = 9;
+    initialAngle = 3;
+
+    for (int i = 0; i < numberOfLightRays; i++) {
+      final radiusRandomness = rand.nextDouble();
+      final randomRadius = (size.shortestSide * .25) +
+          size.shortestSide * 0.3 * radiusRandomness;
+
+      double angle =
+          initialAngle + 100 / numberOfLightRays * (i) - innerRaySize;
+      lightRayList.add(
+        LightRay(
+          center: center,
+          startAngle: angle,
+          endAngle: angle + innerRaySize * 2,
+          radius: randomRadius,
+          innerRaySize: innerRaySize,
+          isGrowing: rand.nextBool(),
+        ),
+      );
+    }
+
+    numberOfLightRays = 9;
+    initialAngle = 10;
+
+    for (int i = 0; i < numberOfLightRays; i++) {
+      final radiusRandomness = rand.nextDouble();
+      final randomRadius = (size.shortestSide * .25) +
+          size.shortestSide * 0.3 * radiusRandomness;
+
+      double angle =
+          initialAngle + 100 / numberOfLightRays * (i) - innerRaySize;
+      lightRayList.add(
+        LightRay(
+          center: center,
+          startAngle: angle,
+          endAngle: angle + innerRaySize * 2,
+          radius: randomRadius,
+          innerRaySize: innerRaySize,
+          isGrowing: rand.nextBool(),
+        ),
+      );
+
+      print(lightRayList.length);
+    }
   }
 
   void drawCircle(Canvas canvas, Size size) {
@@ -224,10 +240,11 @@ class LightRay {
     required this.endAngle,
     required this.innerRaySize,
     required this.center,
+    required this.isGrowing,
   });
 
   ///Valor do tamanho do raio de luz a partir de [center].
-  final double radius;
+  double radius;
 
   ///Valor entre [0-100]. Uma porcentagem de 2*pi, em sentido horário.
   final double startAngle;
@@ -243,8 +260,28 @@ class LightRay {
   ///Indica o ponto inicial do raio de luz, o ponto mais branco e fino do raio.
   final Offset center;
 
+  //Controls weather the light ray must grow or shrink.
+  bool isGrowing = true;
+
   Path get outerPath {
     return _getRayPath(startAngle: startAngle, endAngle: endAngle);
+  }
+
+  void increaseRadius(
+      double value, double maxRadiusSize, double minRadiusSize) {
+    if (isGrowing) {
+      if (radius + value > maxRadiusSize) {
+        isGrowing = false;
+        radius -= value;
+      } else
+        radius += value;
+    } else {
+      if (radius - value < minRadiusSize) {
+        isGrowing = true;
+        radius += value;
+      } else
+        radius -= value;
+    }
   }
 
   Path get innerRayPath {
